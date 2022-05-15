98b1c3df1623e0849f1b9f47f92c520a564b779d
song2000s@office.yuhan.ac.kr <song2000s@office.yuhan.ac.kr>
2022-05-15 21:04:56 +0900

    [ha] 10주차 과제

---
+++ pp007school/Player.hpp
@@ -0,0 +1,22 @@
+class Player
+{
+public:
+	int x, y;
+	Player()
+	{
+		x = 20;
+		y = 7;
+	}
+	~Player()
+	{
+
+	}
+	void isKeyPressed()
+	{
+		y = 5;
+	}
+	void isKeyUnpressed()
+	{
+		y = 7;
+	}
+};
